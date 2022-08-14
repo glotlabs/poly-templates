1e2c237b338ba0c6a7d5c3af514e6549b3a23b94
Petter Rasmussen <petter@hask.no>
2022-08-14 19:13:17 +0300

    Use impl_wasm_page macro

--- counter-tailwind/myapp_wasm/src/home_page.rs
+++ counter-tailwind/myapp_wasm/src/home_page.rs
@@ -1,40 +1,15 @@
 use myapp_lib::home_page;
 use polyester::page::wasm;
 use polyester::page::Page;
+use polyester_macro::impl_wasm_page;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct HomePage(home_page::HomePage);
 
-#[wasm_bindgen]
-impl HomePage {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> Result<HomePage, JsValue> {
-        Ok(HomePage(home_page::HomePage {}))
-    }
+impl_wasm_page!(HomePage);
 
-    #[wasm_bindgen(js_name = id)]
-    pub fn id(&self) -> Result<String, JsValue> {
-        Ok(self.0.id().to_string())
-    }
-
-    #[wasm_bindgen(js_name = init)]
-    pub fn init(&self) -> Result<JsValue, JsValue> {
-        wasm::init(&self.0)
-    }
-
-    #[wasm_bindgen(js_name = viewBody)]
-    pub fn view_body(&self, js_model: &JsValue) -> Result<String, JsValue> {
-        wasm::view_body(&self.0, js_model)
-    }
-
-    #[wasm_bindgen(js_name = getSubscriptions)]
-    pub fn get_subscriptions(&self, js_model: &JsValue) -> Result<JsValue, JsValue> {
-        wasm::get_subscriptions(&self.0, js_model)
-    }
-
-    #[wasm_bindgen(js_name = update)]
-    pub fn update(&self, js_msg: &JsValue, js_model: &JsValue) -> Result<JsValue, JsValue> {
-        wasm::update(&self.0, js_msg, js_model)
-    }
+#[wasm_bindgen(js_name = homePage)]
+pub fn new() -> Result<HomePage, JsValue> {
+    Ok(HomePage(home_page::HomePage {}))
 }
--- counter-tailwind/myapp_web/src/home_page.ts
+++ counter-tailwind/myapp_web/src/home_page.ts
@@ -1,12 +1,12 @@
 import init from "../wasm/myapp.js";
-import { HomePage } from "../wasm/myapp";
+import { homePage } from "../wasm/myapp";
 import { Polyester } from "polyester";
 import { defaultDebugConfig } from "polyester/src/logger";
 
 (async () => {
   await init("./wasm/myapp_bg.wasm");
 
-  const polyester = new Polyester(new HomePage(), {
+  const polyester = new Polyester(homePage(), {
     loggerConfig: defaultDebugConfig(),
   });
 
