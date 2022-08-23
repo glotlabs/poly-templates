59cb125d43e75f432f4834735f962325ff5b533f
Petter Rasmussen <petter@hask.no>
2022-08-23 23:29:23 +0300

    Rename PageEffect to AppEffect

--- counter-tailwind/myapp_core/src/home_page.rs
+++ counter-tailwind/myapp_core/src/home_page.rs
@@ -15,12 +15,12 @@ pub struct Model {
 
 pub struct HomePage {}
 
-impl Page<Model, Msg, PageEffect> for HomePage {
+impl Page<Model, Msg, AppEffect> for HomePage {
     fn id(&self) -> DomId {
         DomId::new("myapp")
     }
 
-    fn init(&self) -> (Model, Effects<Msg, PageEffect>) {
+    fn init(&self) -> (Model, Effects<Msg, AppEffect>) {
         let model = Model { count: 0 };
 
         let effects = vec![];
@@ -28,14 +28,14 @@ impl Page<Model, Msg, PageEffect> for HomePage {
         (model, effects)
     }
 
-    fn subscriptions(&self, _model: &Model) -> browser::Subscriptions<Msg, PageEffect> {
+    fn subscriptions(&self, _model: &Model) -> browser::Subscriptions<Msg, AppEffect> {
         vec![
             browser::on_click(&Id::Increment, Msg::Increment),
             browser::on_click(&Id::Decrement, Msg::Decrement),
         ]
     }
 
-    fn update(&self, msg: &Msg, model: &mut Model) -> Result<Effects<Msg, PageEffect>, String> {
+    fn update(&self, msg: &Msg, model: &mut Model) -> Result<Effects<Msg, AppEffect>, String> {
         match msg {
             Msg::Increment => {
                 model.count += 1;
@@ -73,7 +73,7 @@ pub enum Msg {
 
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub enum PageEffect {}
+pub enum AppEffect {}
 
 fn view_head() -> maud::Markup {
     html! {
