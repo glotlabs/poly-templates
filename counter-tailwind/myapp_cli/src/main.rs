bc9e9edd74db6f511a4ad7f4663c99edd830547c
Petter Rasmussen <petter@hask.no>
2022-08-24 07:43:04 +0300

    Add helper function for printing html

--- counter-tailwind/myapp_cli/src/main.rs
+++ counter-tailwind/myapp_cli/src/main.rs
@@ -5,17 +5,21 @@ use std::env;
 fn main() {
     let args_: Vec<String> = env::args().collect();
     let args: Vec<&str> = args_.iter().map(|s| s.as_ref()).collect();
-    let page = home_page::HomePage {};
 
     match args[1..] {
         ["home_page"] => {
-            let (model, _effects) = page.init();
-            let page = page.view(&model);
-            println!("{}", page.to_markup().into_string());
+            let page = home_page::HomePage {};
+            print_html(page);
         }
 
         _ => {
             println!("Invalid command");
         }
     }
+}
+
+fn print_html<Model, Msg, AppEffect>(page: impl Page<Model, Msg, AppEffect>) {
+    let (model, _effects) = page.init();
+    let page = page.view(&model);
+    println!("{}", page.to_markup().into_string());
 }
