26b908a45ac7cba2db8f00efc552b0ca46fe7ab4
Petter Rasmussen <petter@hask.no>
2022-08-28 09:18:03 +0300

    Update post build script

--- counter-tailwind/post_build.sh
+++ counter-tailwind/post_build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
 
-# Generate html
-cargo run -p myapp_cli -- home_page > dist/index.html
+env="$1"
+event="$2"
+target="debug"
+
+
+if [[ "$env" == "release" ]]; then
+    target="release"
+fi
+
+
+if [[ "$event" == "after_asset_hash" || "$env" == "dev" ]]; then
+    # Generate html
+    ./target/$target/myapp_cli home_page > dist/index.html
+fi
